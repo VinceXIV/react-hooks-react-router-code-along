55e29e04107b5c7a45f5bbd3ce02c7a5d42aef4e
Vincent <vincent.makokha@student.moringaschool.com>
2022-09-27 17:02:34 +0300

    Add additional routes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-// .src/index.js
-
+// ./src/index.js
 import React from "react";
 import ReactDOM from "react-dom";
-// Step 1. Import react-router functions
 import { BrowserRouter, Route } from "react-router-dom";
 
 function Home() {
@@ -13,11 +11,42 @@ function Home() {
   );
 }
 
-// Step 2. Change so router is coordinating what is displayed
+function About() {
+  return (
+    <div>
+      <h1>This is my about component!</h1>
+    </div>
+  );
+}
+
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
+
+
 ReactDOM.render(
   <BrowserRouter>
     <Route path="/">
       <Home />
+    </Route>
+    <Route path="/about">
+      <About />
+    </Route>
+    <Route path="/login">
+      <Login />
     </Route>
   </BrowserRouter>,
   document.getElementById("root")
